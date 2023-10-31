3751dc99cd2f47f4f6627302548be258ddf5e732
Shiv Om Pal <shivompal@gmail.com>
2023-10-31 09:10:25 +0530

    Added functionality to create invoice

---
+++ app/ui/demo.tsx
@@ -0,0 +1,12 @@
+// Server Component
+export default function Page() {
+  // Action
+  async function create(formData: FormData) {
+    "use server";
+
+    // Logic to mutate data...
+  }
+
+  // Invoke the action using the "action" attribute
+  return <form action={create}>...</form>;
+}
